9d37c0999fe5a516a4acea430c7bae6b5128f792
Ильфат Лотфуллин <IILotfullin@stud.kpfu.ru>
2022-03-15 02:01:10 +0300

    Добавьте файлы проекта.

---
+++ Portfolio.Misc/Models/EmailModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PortfolioMisc.Models
+{
+    public class EmailModel
+    {
+        public Guid Id { get; set; }
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public string? mobileNumber { get; set; }
+        public string? message { get; set; }
+    }
+}
---
+++ Portfolio.Misc/Services/EmailService/EmailService.cs
@@ -0,0 +1,26 @@
+﻿using MimeKit;
+using PortfolioMisc.Models;
+
+namespace PortfolioMisc.Services.EmailService
+{
+    public class EmailService : IEmailService
+    {
+        public void SendGmail(EmailModel emailModel)
+        {
+            MimeMessage message = new MimeMessage();
+            message.From.Add(new MailboxAddress($"{emailModel.name}", ""));
+            message.To.Add(new MailboxAddress("", "ilfatka.ytka@gmail.com"));
+            message.Body = new BodyBuilder() { HtmlBody = $"<div>Телефон для связи: {emailModel.mobileNumber}</div><div>{emailModel.message}</div>" }.ToMessageBody();
+
+            using (MailKit.Net.Smtp.SmtpClient client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 465, true);
+                client.Authenticate("nice.blaze@gmail.com", "Arbuuze1");
+                client.Send(message);
+
+                client.Disconnect(true);
+            }
+        }
+
+    }
+}
